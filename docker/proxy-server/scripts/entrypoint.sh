844ab8d79a928070c61474c3288374861f2b1072
msalejandrojose <msalejandrojose7@gmail.com>
2023-07-20 01:17:14 +0200

    f52

--- docker/proxy-server/scripts/entrypoint.sh
+++ docker/proxy-server/scripts/entrypoint.sh
@@ -4,7 +4,7 @@ mode=$1
 
 if [ "$mode" != "local" ]; then
     echo "Import secrets"
+    ls ../usr/share/nginx/project/secret.env
+    cat ../usr/share/nginx/project/secret.env
     export $(cat ../usr/share/nginx/project/secret.env | xargs)
 fi
-
-
