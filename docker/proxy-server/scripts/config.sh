6ba850d7c8d2e051f9a0596e9357d2a2beeedc6c
msalejandrojose <msalejandrojose7@gmail.com>
2023-07-19 23:11:54 +0200

    f22

--- docker/proxy-server/scripts/config.sh
+++ docker/proxy-server/scripts/config.sh
@@ -3,11 +3,12 @@
 export HCP_CLIENT_ID=$1
 #"V2zh4kn7372DGDyqqKhK2DqEpyad8Ai6"
 export HCP_CLIENT_SECRET=$2
+export APP_NAME=test-application
 #"Xtg-brkkEErIOJMsQbx__ZTQVHsXGKUzW4FH1TbD_V1XupzXPRSfV5wR_P2iuwGI"
 echo "HOLAAA"
 #
 vlt login
-vlt secrets --app-name="test-application"
+vlt secrets
 #vlt config ./vlt-config.yaml
 #vlt config init test-application
 
